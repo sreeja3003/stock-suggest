913c664f14c32f58963e8908692805cb2e4f7d6e
Rohit Maheshwari <rohit.maheshwari@sjsu.edu>
2019-05-16 21:17:12 -0700

    Adding Python/Flask based backend
    POST API '/getData' to fetch stock details

---
+++ backend/README.md
@@ -0,0 +1,45 @@
+# Backend(Python Server)
+
+A python based server to fetch stock data based on Investment Strategies.
+
+## Prerequisites
+
+* Linux, Mac, or Windows machine
+* Python
+* Flask web framework
+
+
+
+## Running the Application
+
+The Flask application is ran with a localhost server (IP address 127.0.0.1:5000).
+
+Flask first needs to be told how to import the application, by setting the ```FLASK_APP``` environmental variable:
+
+```
+$ export FLASK_APP=stock-portfolio-suggestion-web-app.py
+```
+
+On Microsoft Windows, use ```set``` instead of ```export``` in the command above.
+
+You can now run the application with the following command:
+
+```
+$ flask run
+
+```
+
+## API Details
+
+```
+POST /getData
+
+```
+
+Form data:
+
+| Field          | Description                                                       | Optional   |
+| -------------- | ----------------------------------------------------------------- | ---------- |
+| `Strategies`   | One or two strategies as array ["Strategy 1","Strategy 2"]        | no         |
+| `Amount`       | Investment Amount                                                 | no         |
+
---
+++ backend/stock-portfolio-suggestion-web-app.py
@@ -0,0 +1,76 @@
+# Import Flask packages
+import requests
+import json
+from flask import Flask, render_template, request, Response
+
+
+
+# Define an instance of Flask object
+DEBUG = True
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+
+""" Stocks for investment strategies"""
+
+## Stocks as per the intense study and survey based on the strategies
+ethical_investing = ["AAPL", "TSLA", "ADBE"]
+growth_investing = ["OXLC", "ECC", "AMD"]
+index_investing = ["VOO", "VTI", "ILTB"]
+quality_investing = ["NVDA", "MU", "CSCO"]
+value_investing = ["INTC", "BABA", "GE"]
+
+def get_stock_quote(stock_list):
+    """Function that calls stock API for each stock to fetch stock details"""
+
+    # Filter defining the data requirement
+    param_filter = '?filter=companyName,latestPrice,latestTime,change,changePercent'
+
+    stock_quote = []
+
+    # Loopping through given stock and appending data to result
+    for ticker in stock_list:
+        # HTTP GET call to stock API
+        resp = requests.get('https://api.iextrading.com/1.0/stock/{}/quote/{}'.format(ticker, param_filter))
+        stock_quote.append(resp.json())
+
+    return stock_quote
+
+@app.route('/getData', methods=['POST'])
+def return_data():
+    Strategies = request.json['Strategies']
+    Amount = request.json['Amount']
+    dataLength = len(Strategies)
+    print dataLength , 'Strategies'
+
+    response = []
+    amt1 = Amount*0.5
+    amt2 = Amount*0.30
+    amt3 = Amount*0.20
+    responseAmount = []
+
+    responseAmount.append(amt1)
+    responseAmount.append(amt2)
+    responseAmount.append(amt3)
+
+    for x in Strategies:
+        if x == "Ethical Investing":
+            response.append(get_stock_quote(ethical_investing))
+        elif x == "Growth Investing":
+            response.append(get_stock_quote(growth_investing))
+        elif x == "Index Investing":
+            response.append(get_stock_quote(index_investing))
+        elif x == "Quality Investing":
+            response.append(get_stock_quote(quality_investing))
+        elif x == "Value Investing":
+            response.append(get_stock_quote(value_investing))
+        else:
+            response.append("Invalid Strategy")
+
+    # get_stock_quote(value)
+    dict1 = {"strategiesResponse": response, "amountResponse": responseAmount}
+    return Response(json.dumps(dict1), mimetype='application/json')
+
+
+if __name__ == "__main__":
+    app.run()
